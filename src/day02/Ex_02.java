839bfed215bd5568b2a8423433eab41a02da1e5c
403 <403@DESKTOP-PEL718I>
2018-09-11 16:14:20 +0900

    first java programming

---
+++ src/day02/Ex_02.java
@@ -0,0 +1,17 @@
+package day02;
+
+public class Ex_02 {
+
+	public static void main(String[] args) {
+		System.out.println("JJJJJJJJJJ     AAAAAAA     VV         VV     AAAAAAA");
+		System.out.println("JJJJJJJJJJ    AAAAAAAAA     V         V     AAAAAAAAA");
+		System.out.println("       JJJ   AAA     AAA    VV       VV    AAA     AAA");
+		System.out.println("       JJJ   AAA     AAA     V       V     AAA     AAA");
+		System.out.println("       JJJ  AAAAAAAAAAAAA     V      V    AAAAAAAAAAAAA");
+		System.out.println("J     JJJ   AAAAAAAAAAAAA      V    V     AAAAAAAAAAAAA");
+		System.out.println("JJ  JJJJ   AAA        AAA       V  V     AAA         AAA");
+		System.out.println(" JJJJJJ   AAA          AAA       VV      AAA         AAA");
+		System.out.println("  JJJJ   AAA            AAA       V     AAA           AAA");
+	} 
+
+}
